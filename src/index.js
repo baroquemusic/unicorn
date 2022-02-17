67449b29c873ff7e53916ebc2604afc12a429415
Gabor Kovacs <90977759+baroquemusic@users.noreply.github.com>
2022-02-17 09:41:16 -1000

    givin up

--- src/index.js
+++ src/index.js
@@ -11,11 +11,6 @@ import blockChain from './assets/glb/block.glb'
 import deepLearning from './assets/glb/deep.glb'
 import extendedReality from './assets/glb/extended.glb'
 import interactive3D from './assets/glb/interact.glb'
-import triangleCentroid from 'triangle-centroid'
-import reindex from 'mesh-reindex'
-import unindex from 'unindex-mesh'
-import loadSvg from 'load-svg'
-import { parse as getSvgPaths } from 'extract-svg-path'
 
 const renderer = new THREE.WebGLRenderer( { antialias: true } )
 renderer.setSize( window.innerWidth, window.innerHeight )
@@ -55,163 +50,7 @@ function onMouseMove( event ) {
 
 }
 
-////////////////////////////////////////
-
-
-
-var parseSVG = require('parse-svg-path')
-var getContours = require('svg-path-contours')
-var cdt2d = require('cdt2d')
-var getBounds = require('bound-points')
-var normalize = require('normalize-path-scale')
-var random = require('random-float')
-var assign = require('object-assign')
-var simplify = require('simplify-path')
-
-const createGeom = require('three-simplicial-complex')(THREE)
-
-function svgMesh3d (svgPath, opt) {
-  if (typeof svgPath !== 'string') {
-    throw new TypeError('must provide a string as first parameter')
-  }
-  
-  opt = assign({
-    delaunay: true,
-    clean: true,
-    exterior: false,
-    randomization: 0,
-    simplify: 0,
-    scale: 1
-  }, opt)
-  
-  var i
-  // parse string as a list of operations
-  var svg = parseSVG(svgPath)
-  
-  // convert curves into discrete points
-  var contours = getContours(svg, opt.scale)
-  
-  // optionally simplify the path for faster triangulation and/or aesthetics
-  if (opt.simplify > 0 && typeof opt.simplify === 'number') {
-    for (i = 0; i < contours.length; i++) {
-      contours[i] = simplify(contours[i], opt.simplify)
-    }
-  }
-  
-  // prepare for triangulation
-  var polyline = denestPolyline(contours)
-  var positions = polyline.positions
-  var bounds = getBounds(positions)
-
-  // optionally add random points for aesthetics
-  var randomization = opt.randomization
-  if (typeof randomization === 'number' && randomization > 0) {
-    addRandomPoints(positions, bounds, randomization)
-  }
-  
-  var loops = polyline.edges
-  var edges = []
-  for (i = 0; i < loops.length; ++i) {
-    var loop = loops[i]
-    for (var j = 0; j < loop.length; ++j) {
-      edges.push([loop[j], loop[(j + 1) % loop.length]])
-    }
-  }
-
-  // this updates points/edges so that they now form a valid PSLG 
-  // if (opt.clean !== false) {
-  //   cleanPSLG(positions, edges)
-  // }
-
-  // triangulate mesh
-  var cells = cdt2d(positions, edges, opt)
-
-  // rescale to [-1 ... 1]
-  if (opt.normalize !== false) {
-    normalize(positions, bounds)
-  }
-
-  // convert to 3D representation and flip on Y axis for convenience w/ OpenGL
-  to3D(positions)
-
-  return {
-    positions: positions,
-    cells: cells
-  }
-}
-
-function to3D (positions) {
-  for (var i = 0; i < positions.length; i++) {
-    var xy = positions[i]
-    xy[1] *= -1
-    xy[2] = xy[2] || 0
-  }
-}
-
-function addRandomPoints (positions, bounds, count) {
-  var min = bounds[0]
-  var max = bounds[1]
-
-  for (var i = 0; i < count; i++) {
-    positions.push([ // random [ x, y ]
-      random(min[0], max[0]),
-      random(min[1], max[1])
-    ])
-  }
-}
-
-function denestPolyline (nested) {
-  var positions = []
-  var edges = []
-
-  for (var i = 0; i < nested.length; i++) {
-    var path = nested[i]
-    var loop = []
-    for (var j = 0; j < path.length; j++) {
-      var pos = path[j]
-      var idx = positions.indexOf(pos)
-      if (idx === -1) {
-        positions.push(pos)
-        idx = positions.length - 1
-      }
-      loop.push(idx)
-    }
-    edges.push(loop)
-  }
-  return {
-    positions: positions,
-    edges: edges
-  }
-}
-
-
-function getAnimationAttributes (positions, cells) {
-  const directions = []
-  const centroids = []
-  for (let i=0; i<cells.length; i++) {
-    const [ f0, f1, f2 ] = cells[i]
-    const triangle = [ positions[f0], positions[f1], positions[f2] ]
-    const center = triangleCentroid(triangle)
-    const dir = new THREE.Vector3().fromArray(center)
-    centroids.push(dir, dir, dir)
-    
-    const random = randomVec3([], Math.random())
-    const anim = new THREE.Vector3().fromArray(random)
-    directions.push(anim, anim, anim)
-  }
-  return {
-    direction: { type: 'v3', value: directions },
-    centroid: { type: 'v3', value: centroids }
-  }
-}
-
-
-
 ///////////////// LOGO
-
-
-
-
 
 const logoLoader = new SVGLoader
 const logo = new THREE.Object3D
@@ -263,38 +102,6 @@ function randomX( out ) {
 	return out
 
 }
-
-let svgPath
-let complex
-
-loadSvg( unicornLogo, (err, svg) => {
-	if(err) throw err
-	svgPath = getSvgPaths( svg )
-	complex = svgMesh3d( svgPath, {
-    scale: 10,
-    simplify: .01
-  } )
-	console.log(complex)
-	complex = reindex(unindex(complex.positions, complex.cells))
-	const attributes = getAnimationAttributes(complex.positions, complex.cells)
-  const geometry = new createGeom(complex)
-  
-  // our shader material
-  const material = new THREE.ShaderMaterial({
-    color: 0xffffff,
-    side: THREE.DoubleSide,
-    vertexShader: vertShader,
-    fragmentShader: fragShader,
-    wireframe: wireframe,
-    transparent: true,
-    attributes: attributes,
-    uniforms: {
-      opacity: { type: 'f', value: 1 },
-      scale: { type: 'f', value: 0 },
-      animate: { type: 'f', value: 0 }
-    }
-  })
-} )
 
 logoLoader.load(
 
